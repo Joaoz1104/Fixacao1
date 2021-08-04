aecd7bc7b57cf215f91985cbbf17421e913c9688
Aluno <Aluno@LAB2-02>
2021-08-04 17:03:15 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/ConversorDeMoeda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ConversorDeMoeda
+    {
+        private readonly double Cotaçao = 5.00;
+        public double Dolar { get; set; }
+        double Valor = 0;
+        public double ValorReais()
+        {
+            Valor = (Cotaçao * Dolar) * 6 / 100 + (Cotaçao * Dolar);
+            return Valor;
+        }
+        public override string ToString()
+        {
+            return $"Valor a ser pago em reais: {ValorReais()}";
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversorDeMoeda conversor = new ConversorDeMoeda();
+
+            Console.WriteLine("A cotação é 5.00");
+           
+            Console.WriteLine("Quantos dólares você vai comprar?");
+            conversor.Dolar = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(conversor);
+        }
+    }
+}
